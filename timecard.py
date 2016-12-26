91e269a2a0e18bbaab8bce3c7aa776fa493456b9
Justin <jrcarlson8@gmail.com>
2016-12-25 21:47:19 -0500

    Frontend and backend revisions

--- timecard.py
+++ timecard.py
@@ -1,4 +1,3 @@
-import arrow
 import datetime
 from flask import Flask, request, session, redirect, url_for, render_template, jsonify, abort
 from flask_sqlalchemy import SQLAlchemy
@@ -7,9 +6,9 @@ app = Flask(__name__)
 app.config.from_pyfile('timecard.cfg')
 db = SQLAlchemy(app)
 
-slot_increment = arrow.get(app.config['SLOTINCREMENT'], 'HH:mm')
-slot_first_start = arrow.get(app.config['SLOTFIRSTSTART'], 'HH:mm')
-slot_last_start = arrow.get(app.config['SLOTLASTSTART'], 'HH:mm')
+slot_increment = app.config['SLOTINCREMENT']
+slot_first_start = datetime.datetime.strptime(app.config['SLOTFIRSTSTART'], '%H:%M')
+slot_last_start = datetime.datetime.strptime(app.config['SLOTLASTSTART'], '%H:%M')
 
 
 class User(db.Model):
@@ -46,24 +45,29 @@ class Timeslot(db.Model):
 
 def hours_range(start_time, end_time, increment):
     curr = start_time
+    increment_delta = datetime.timedelta(minutes=increment)
     while curr <= end_time:
         yield curr
-        curr = curr.shift(minutes=+increment.minute)
+        curr = curr + increment_delta
 
 
 @app.route('/')
-@app.route('/index')
+#@app.route('/index')
 def show_timecard():
     #if session['logged_in']:
     # session should contain info like privilege, username, name
     #session['username'] = app.config['USERNAME']
 
+    # will be supplied by cas
+    session['CAS_USERNAME'] = 'carlsj4'
+
     # days of week 0-6
     days=range(7)
-    # times representing start of each cell
+
+    # times representing start of each cell eg. 8:30
     times = [slot for slot in hours_range(slot_first_start, slot_last_start, slot_increment)]
 
-    return render_template('layout.html', slot_increment=slot_increment.minute, slot_first_start=times[0].format("HHmm"), days=days, times=times)
+    return render_template('layout.html', slot_increment=slot_increment, slot_first_start=times[0].strftime("%H%m"), days=days, times=times)
     #else:
     #return redirect(url_for('login'))
 
@@ -84,7 +88,11 @@ def update():
 
     elif request.method == 'POST':
         content = request.get_json()
+
+        # Add timestamps in 'selected'
         if (len(content['selected']) > 0):
+            # TODO: Ignore duplicate timestamps and those in locked timecards (prior to lock date) unless admin
+            # maybe have a lock date and refuse changes to timestamps prior, maybe lock by day/week
             u.slots.extend([Timeslot(timestamp=t) for t in content['selected']])
         
         # Delete timestamps in 'unselected'
@@ -117,9 +125,13 @@ def update():
 #    session.pop('logged_in', None)
 #    return render_template('logout.html')
 
+#@app.errorhandler(404)
+#def page_not_found(e):
+#    return render_template('404.html'), 404
 
 def init_db():
     # for use with command line argument to reset database
+    # remember to change db in config
 
     db.drop_all()
     db.create_all()
